5837e4995c0d5bf18be6c742f9026b4244a75b27
ower <financewithcoffee@gmail.com>
2025-01-29 22:32:06 +0900

    댓글 다는 부분 분리했던 dart 파일 을 병합함

--- lib/pages/post/view_post_screen.dart
+++ lib/pages/post/view_post_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_application_1/common/themes/colors.dart';
 import 'package:flutter_application_1/common/utils/dateTimeUtil.dart';
 import 'package:flutter_application_1/common/widgets/errorBoundary.dart';
-import 'package:flutter_application_1/data/repositories/firestore_repository.dart';
 import 'package:flutter_application_1/common/widgets/commonProgressIndicator.dart';
-import 'package:flutter_application_1/pages/post/post_comments.dart';
+import 'package:flutter_application_1/data/repositories/firestore_repository.dart';
 import 'package:go_router/go_router.dart';
 import 'package:share_plus/share_plus.dart';
 
@@ -18,30 +18,47 @@ class ViewPostScreen extends StatefulWidget {
 }
 
 class _ViewPostScreenState extends State<ViewPostScreen> {
-  bool isLiked = false; // 좋아요 상태
-  int likes = 0; // 좋아요 개수
-  int views = 0; // 조회수
+  bool isLiked = false;
+  int likes = 0;
+  int views = 0;
 
   final ValueNotifier<bool> isLikedNotifier = ValueNotifier(false);
   final ValueNotifier<int> likesNotifier = ValueNotifier(0);
+
+  final TextEditingController _commentController = TextEditingController();
+  final ValueNotifier<bool> isTextNotEmpty = ValueNotifier(false);
+  FirestoreService firestoreService = FirestoreService();
+
+  @override
+  void initState() {
+    super.initState();
+    firestoreService = FirestoreService();
+    _commentController.addListener(() {
+      isTextNotEmpty.value = _commentController.text.isNotEmpty;
+    });
+  }
+
+  @override
+  void dispose() {
+    _commentController.dispose();
+    isTextNotEmpty.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final String postId = widget.postId;
 
-    print("Current ViewPost Id: " + postId);
-    final FirestoreService firestoreService = FirestoreService();
-
     return ErrorBoundary(
       child: Scaffold(
         appBar: AppBar(
+          scrolledUnderElevation: 0,
           leading: IconButton(
             icon: const Icon(Icons.arrow_back),
             onPressed: () {
               try {
                 GoRouter.of(context).pop();
               } catch (e) {
-                print(e);
                 context.go('/mainCommunity');
               }
             },
@@ -51,88 +68,108 @@ class _ViewPostScreenState extends State<ViewPostScreen> {
             IconButton(
               icon: const Icon(Icons.share),
               onPressed: () {
-                final String shareUrl = '';
-                Share.share('Check out this post: $shareUrl');
+                Share.share('Check out this post: ');
               },
             ),
           ],
         ),
-        body: FutureBuilder<Map<String, dynamic>?>(
-          future: _incrementViewsAndFetchPost(firestoreService, postId),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CommonProgressIndicator());
-            } else if (snapshot.hasError) {
-              return const Center(child: Text('일시적 오류입니다. 다시 시도해주세요.'));
-            } else if (snapshot.data == null) {
-              return const Center(child: Text('게시글을 찾을 수 없습니다.'));
-            } else {
-              final data = snapshot.data!;
-              final String category = data['category'] ?? '미분류';
-              final String title = data['title'] ?? '제목 없음';
-              final String content = data['content'] ?? '내용 없음';
-              final String author = data['author'] ?? '익명';
-              final String time = formatTimestamp(data['register_time']);
-              final int comments = data['comments_cnt'] ?? 0;
-              views = data['views_cnt'] ?? 0;
-              likes = data['likes_cnt'] ?? 0; // 좋아요 초기화
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 60.0),
+            child: Column(
+              children: [
+                FutureBuilder<Map<String, dynamic>?>(
+                  future: _incrementViewsAndFetchPost(firestoreService, postId),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CommonProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      return const Center(child: Text('일시적 오류입니다. 다시 시도해주세요.'));
+                    } else if (snapshot.data == null) {
+                      return const Center(child: Text('게시글을 찾을 수 없습니다.'));
+                    } else {
+                      final data = snapshot.data!;
+                      final String category = data['category'] ?? '미분류';
+                      final String title = data['title'] ?? '제목 없음';
+                      final String content = data['content'] ?? '내용 없음';
+                      final String author = data['author'] ?? '익명';
+                      final String time =
+                          formatTimestamp(data['register_time']);
+                      final int comments = data['comments_cnt'] ?? 0;
+                      views = data['views_cnt'] ?? 0;
+                      likes = data['likes_cnt'] ?? 0;
 
-              likesNotifier.value = data['likes_cnt'] ?? 0;
-              isLikedNotifier.value = false; // 기본 값 초기화
+                      return Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            _buildCategoryTag(category),
+                            const SizedBox(height: 8),
+                            Text(title,
+                                style: const TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold)),
+                            const SizedBox(height: 4),
+                            Text('$author | $time',
+                                style: const TextStyle(
+                                    color: MyColors.subFontColor,
+                                    fontSize: 14)),
+                            Text(content, style: const TextStyle(fontSize: 16)),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                _buildLikesSection(firestoreService, postId),
+                                Text('댓글 $comments   조회 $views회'),
+                              ],
+                            ),
+                            // 댓글 리스트
+                            StreamBuilder<QuerySnapshot>(
+                              stream: firestoreService
+                                  .getCommentsStream(widget.postId),
+                              builder: (context, snapshot) {
+                                if (snapshot.connectionState ==
+                                    ConnectionState.waiting) {
+                                  return const Center(
+                                      child: CommonProgressIndicator());
+                                }
+                                if (!snapshot.hasData ||
+                                    snapshot.data!.docs.isEmpty) {
+                                  return const Center(
+                                      child: Text('첫 댓글을 남겨주세요.'));
+                                }
 
-              return SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      _buildCategoryTag(category),
-                      const SizedBox(height: 8),
-                      Text(
-                        title,
-                        style: const TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(height: 4),
-                      Text(
-                        '$author | $time',
-                        style: const TextStyle(
-                            color: MyColors.subFontColor, fontSize: 14),
-                      ),
-                      const Divider(height: 24, thickness: 1),
-                      Text(
-                        content,
-                        style: const TextStyle(fontSize: 16),
-                      ),
-                      const Divider(height: 24, thickness: 1),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          _buildLikesSection(firestoreService, postId),
-                          Text('댓글 $comments   조회 $views회'),
-                        ],
-                      ),
-                      const Divider(height: 24, thickness: 1),
-                      PostComments(postId: postId),
-                    ],
-                  ),
+                                return Column(
+                                  children: snapshot.data!.docs.map((doc) {
+                                    return ListTile(
+                                      title: Text(doc['text']),
+                                      subtitle: Text(
+                                          formatRelativeTime(doc['timestamp'])),
+                                    );
+                                  }).toList(),
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                    }
+                  },
                 ),
-              );
-            }
-          },
+              ],
+            ),
+          ),
         ),
+        bottomSheet: _buildCommentInputField(),
       ),
     );
   }
 
-  /// 조회수를 증가시키고 게시글 데이터를 가져오는 메서드
   Future<Map<String, dynamic>?> _incrementViewsAndFetchPost(
       FirestoreService firestoreService, String postId) async {
     await firestoreService.incrementViews('posts', postId);
     return await firestoreService.getPostById('posts', postId);
   }
 
-  // 좋아요 UI와 동작 분리
   Widget _buildLikesSection(FirestoreService firestoreService, String postId) {
     return Row(
       children: [
@@ -144,7 +181,6 @@ class _ViewPostScreenState extends State<ViewPostScreen> {
                 isLiked ? Icons.thumb_up_alt : Icons.thumb_up_alt_outlined,
               ),
               onPressed: () {
-                // 즉시 값 변경
                 final newValue = !isLiked;
                 final newLikes = newValue
                     ? likesNotifier.value + 1
@@ -170,26 +206,79 @@ class _ViewPostScreenState extends State<ViewPostScreen> {
     );
   }
 
-  @override
-  void dispose() {
-    isLikedNotifier.dispose();
-    likesNotifier.dispose();
-    super.dispose();
-  }
-
   Widget _buildCategoryTag(String category) {
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
       decoration: BoxDecoration(
         color: MyColors.mainColor,
         borderRadius: BorderRadius.circular(8),
-        border: Border.all(
-          color: Colors.transparent,
-        ),
       ),
-      child: Text(
-        category,
-        style: const TextStyle(color: MyColors.mainFontColor),
+      child:
+          Text(category, style: const TextStyle(color: MyColors.mainFontColor)),
+    );
+  }
+
+  Widget _buildCommentInputField() {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      color: Colors.white,
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _commentController,
+              decoration: const InputDecoration(
+                hintText: '댓글을 남겨주세요.',
+                counterText: '',
+                isDense: true,
+                contentPadding: EdgeInsets.all(10),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: MyColors.grey),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: MyColors.mainlightColor,
+                    width: 2.0,
+                  ),
+                ),
+              ),
+              maxLength: 500,
+            ),
+          ),
+          const SizedBox(width: 8),
+          ValueListenableBuilder<bool>(
+            valueListenable: isTextNotEmpty,
+            builder: (context, isNotEmpty, child) {
+              return ElevatedButton(
+                onPressed: isNotEmpty
+                    ? () async {
+                        await firestoreService.addComment(
+                          widget.postId,
+                          _commentController.text,
+                        );
+                        _commentController.clear();
+                      }
+                    : null,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor:
+                      isNotEmpty ? MyColors.mainColor : MyColors.grey,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: Text(
+                  '남기기',
+                  style: TextStyle(
+                    color: MyColors.mainFontColor,
+                    fontSize: 14,
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
